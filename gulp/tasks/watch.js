6f2fa7dc942ea572030d88f1dc077bdf5bdb1f19
Stef Elliott <stef.elliott@6sm.co.uk>
2019-03-12 17:17:22 +0000

    update to footer to include lots of code

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -4,24 +4,24 @@ browserSync = require('browser-sync').create();
 
 gulp.task('watch', function() {
 
-    browserSync.init({
-      notify: false,
-      server: {
-        baseDir: "app"
-      }
-    });
-    
-      watch('./app/index.html', function() {
-        browserSync.reload();
-      });
-    
-      watch('./app/assets/styles/**/*.css', function() {
-        gulp.start('cssInject');
-      });
-    
-    });
-    
-    gulp.task('cssInject', ['styles'],function() {
-      return gulp.src('./app/temp/styles/styles.css')
-      .pipe(browserSync.stream());
-    });
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: "app"
+    }
+  });
+
+  watch('./app/index.html', function() {
+    browserSync.reload();
+  });
+
+  watch('./app/assets/styles/**/*.css', function() {
+    gulp.start('cssInject');
+  });
+
+});
+
+gulp.task('cssInject', ['styles'], function() {
+  return gulp.src('./app/temp/styles/styles.css')
+    .pipe(browserSync.stream());
+});
